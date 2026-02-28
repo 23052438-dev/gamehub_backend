588e655578b560aa80d262b78a1dac271a29674a
sovikburma <sovikburma+github@gmail.com>
2026-02-28 16:43:50 +0530

    Initial backend setup

--- server.js
+++ server.js
@@ -1,5 +1,3 @@
-require("dotenv").config();
-
 const express = require("express");
 const mysql = require("mysql2");
 const cors = require("cors");
@@ -10,11 +8,10 @@ app.use(cors());
 app.use(express.json());
 
 const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT
+    host: "localhost",
+    user: "root",
+    password: "password",
+    database: "gamehub"
 });
 
 db.connect((err) => {
@@ -29,8 +26,6 @@ app.get("/", (req, res) => {
     res.send("GameHub Backend Running");
 });
 
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-    console.log("Server running");
+app.listen(5000, () => {
+    console.log("Server running on port 5000");
 });
